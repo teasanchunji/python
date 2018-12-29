6e4137d5b6d36f4d11d069897b196430041698d0
teasanchunji <teasanchunji@gmail.com>
2018-12-29 10:58:48 -0500

    Update show-clock.py

--- show-clock.py
+++ show-clock.py
@@ -23,12 +23,6 @@ for device in devices:
         print("Connecting to device", device)
         connection = netmiko.ConnectHandler(ip=device, device_type=device_type, username=username, password=password)
         print(connection.send_command("show ip int brie\n"))
-        print(connection.send_command("enable\n"))
-        print(connection.send_command("configure terminal\n"))
-        print(connection.send_command("int loop 0\n"))
-        print(connection.send_command("ip address 1.1.1.1 255.255.255.255\n"))
-        print(connection.send_command("end\n"))
-        print(connection.send_command("show ip int brie\n"))
         connection.disconnect()
 
     except netmiko_exception as e:
